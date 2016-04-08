0851851664481327def35a9fb9ceebb7ba91a105
I1138 <ianscookie2@gmail.com>
2016-04-08 10:11:28 -0400

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,6 +1,9 @@
-import subprocess
+import subprocess # subproccess is used to make command line calls
 
+
+#get ip range
 adress = str(input("enter ip adress range: "))
+#parse input, and assign variables.
 adress = str.split(adress,sep=".")
 #print(adress)
 scan = adress.pop()
@@ -19,10 +22,14 @@ middle = adress.pop(0)
 #print(middle)
 bottom = adress.pop(0)
 #print(bottom)
+
+#main loop
 for i in range(last-first+1):
         received = "false"
+        #cmd-line call
         cmd = "ping /n 1 " + top + "." + middle + "." + bottom + "." + str(current) + " | findstr Reply"
         text = subprocess.run(cmd,stderr=subprocess.STDOUT,shell=True,stdout=subprocess.PIPE)
+        #parse command output
         text = str(text)
         #print(text)
         textString = str.split(text,sep=":")
@@ -31,8 +38,8 @@ for i in range(last-first+1):
         textString = str.split(textString, sep=".")
         textString = textString.pop()
         #print(textString)
+        #print output to console.
         if textString== str(current):
                 received = "true"
         print(top+"."+middle+"."+bottom+"." + str(current) + ": " + received)
         current += 1
-
