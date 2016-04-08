549ac6e066af1e24ca6694d8543207f3de88556e
I1138 <ianscookie2@gmail.com>
2016-04-07 20:37:20 -0400

    Added files via upload

---
+++ scan.py
@@ -0,0 +1,38 @@
+import subprocess
+
+adress = str(input("enter ip adress range: "))
+adress = str.split(adress,sep=".")
+#print(adress)
+scan = adress.pop()
+scan = str.split(scan, sep="-")
+#print(scan)
+first = scan.pop(0)
+#print(first)
+first = int(first)
+last = scan.pop()
+#print(last)
+last = int(last)
+current = first
+top = adress.pop(0)
+#print(top)
+middle = adress.pop(0)
+#print(middle)
+bottom = adress.pop(0)
+#print(bottom)
+for i in range(last-first+1):
+        received = "false"
+        cmd = "ping /n 1 " + top + "." + middle + "." + bottom + "." + str(current) + " | findstr Reply"
+        text = subprocess.run(cmd,stderr=subprocess.STDOUT,shell=True,stdout=subprocess.PIPE)
+        text = str(text)
+        #print(text)
+        textString = str.split(text,sep=":")
+        textString = textString.pop(0)
+        #print(textString)
+        textString = str.split(textString, sep=".")
+        textString = textString.pop()
+        #print(textString)
+        if textString== str(current):
+                received = "true"
+        print(top+"."+middle+"."+bottom+"." + str(current) + ": " + received)
+        current += 1
+
